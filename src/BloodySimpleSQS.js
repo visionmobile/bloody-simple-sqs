da7e0ccb02eae58e8be5680ef91962ce8eac3735
Dimitrios C. Michalakos <dimitris@jmike.gr>
2014-09-26 17:51:58 +0300

    Implement a readable stream API.

--- src/BloodySimpleSQS.js
+++ src/BloodySimpleSQS.js
@@ -1,5 +1,7 @@
 var events = require('events'),
   util = require('util'),
+  Readable = require('stream').Readable,
+  _ = require('lodash'),
   AWS = require('aws-sdk'),
   Promise = require('bluebird');
 
@@ -9,15 +11,14 @@ var events = require('events'),
  * @param {string} options.queueName the name of the queue to connect to.
  * @param {string} options.accessKeyId your AWS access key ID.
  * @param {string} options.secretAccessKey your AWS secret access key.
- * @param {string} [options.region=us-east-1] the region to send service requests to.
+ * @param {string} [options.region=us-east-1] the AWS region of the queue.
  * @constructor
  */
 function BloodySimpleSQS(options) {
   var queueName, accessKeyId, secretAccessKey, region;
 
-  // validate "options" param
-  if (typeof options !== 'object') {
-    throw new Error('Invalid or unspecified "options" param');
+  if (!_.isObject(options)) {
+    throw new Error('Invalid options param; expected object, received ' + typeof(options));
   }
 
   queueName = options.queueName;
@@ -25,10 +26,21 @@ function BloodySimpleSQS(options) {
   secretAccessKey = options.secretAccessKey;
   region = options.region || 'us-east-1';
 
-  if (typeof queueName !== 'string') throw new Error('Invalid or unspecified "queueName" option');
-  if (typeof accessKeyId !== 'string') throw new Error('Invalid or unspecified "accessKeyId" option');
-  if (typeof secretAccessKey !== 'string') throw new Error('Invalid or unspecified "secretAccessKey" option');
-  if (typeof region !== 'string') throw new Error('Invalid or unspecified "region" option');
+  if (!_.isString(queueName)) {
+    throw new Error('Invalid queueName option; expected string, received ' + typeof(queueName));
+  }
+
+  if (!_.isString(accessKeyId)) {
+    throw new Error('Invalid accessKeyId option; expected string, received ' + typeof(accessKeyId));
+  }
+
+  if (!_.isString(secretAccessKey)) {
+    throw new Error('Invalid secretAccessKey option; expected string, received ' + typeof(secretAccessKey));
+  }
+
+  if (!_.isString(region)) {
+    throw new Error('Invalid region option; expected string, received ' + typeof(region));
+  }
 
   this.queueName = queueName;
 
@@ -44,11 +56,13 @@ function BloodySimpleSQS(options) {
   this.setMaxListeners(99);
 
   // load data from aws and emit ready
-  this._getQueueUrl().bind(this).then(function (url) {
-    this.queueUrl = url;
-    this.isReady = true;
-    this.emit('ready');
-  });
+  this._getQueueUrl()
+    .bind(this)
+    .then(function (url) {
+      this.queueUrl = url;
+      this.isReady = true;
+      this.emit('ready');
+    });
 }
 
 // BloodySimpleSQS extends the EventEmitter class
@@ -60,11 +74,9 @@ util.inherits(BloodySimpleSQS, events.EventEmitter);
  * @private
  */
 BloodySimpleSQS.prototype._getQueueUrl = function () {
-  var self = this,
-    resolver;
+  var self = this, resolver;
 
   resolver = function(resolve, reject) {
-
     var params = {
       QueueName: self.queueName
     };
@@ -74,43 +86,35 @@ BloodySimpleSQS.prototype._getQueueUrl = function () {
 
       resolve(response.QueueUrl);
     });
-
   };
 
   return new Promise(resolver);
 };
 
 /**
- * Appends a new message, with the designated body, at the end of the queue.
- * @param {(boolean|string|number|object|null)} body the body of the message.
+ * Appends a new message, with the designated payload, at the end of the queue.
+ * @param {(boolean|string|number|object|null)} payload the message payload.
  * @param {function} [callback] an optional callback function, i.e. function (err, response).
  * @return {Promise}
  */
-BloodySimpleSQS.prototype.add = function (body, callback) {
-  var self = this,
-    resolver;
+BloodySimpleSQS.prototype.add = function (payload, callback) {
+  var self = this, resolver;
 
   resolver = function(resolve, reject) {
-
     var params = {
       QueueUrl: self.queueUrl,
-      MessageBody: JSON.stringify(body)
+      MessageBody: JSON.stringify(payload)
     };
 
     self.sqs.sendMessage(params, function(err, response) {
-      var message;
-
       if (err) return reject(err);
 
-      message = {
+      resolve({
         id: response.MessageId,
-        body: body,
+        body: payload,
         md5: response.MD5OfMessageBody
-      };
-
-      resolve(message);
+      });
     });
-
   };
 
   return new Promise(function(resolve, reject) {
@@ -127,47 +131,53 @@ BloodySimpleSQS.prototype.add = function (body, callback) {
 /**
  * Retrieves, but does not remove, the head of the queue.
  * @param {object} [options] optional request options.
- * @param {number} [options.timeout=0] number of seconds to wait until a message arrives in the queue, must be >= 0 and <= 20.
- * @param {function} [callback] an optional call back function, i.e. function (err, message, receiptHandle).
+ * @param {number} [options.timeout=0] number of seconds to wait until a message arrives in the queue; must be between 0 and 20.
+ * @param {number} [options.limit=1] maximum number of messages to return; must be between 1 and 10.
+ * @param {function} [callback] an optional call back function, i.e. function (err, receiptHandle).
  * @return {Promise}
  */
 BloodySimpleSQS.prototype.peek = function (options, callback) {
-  var self = this,
-    resolver;
+  var self = this, resolver;
 
   // handle optional "options" param
-  if (typeof options === 'function') {
+  if (_.isFunction(options)) {
     callback = options;
     options = {};
-  } else if (options === undefined) {
+  } else if (_.isUndefined(options)) {
     options = {};
   }
 
-  resolver = function(resolve, reject) {
+  options.timeout = options.timeout || 0;
+  options.limit = options.limit || 1;
 
+  resolver = function(resolve, reject) {
     var params = {
       QueueUrl: self.queueUrl,
-      MaxNumberOfMessages: 1,
-      WaitTimeSeconds: options.timeout || 0
+      MaxNumberOfMessages: options.limit,
+      WaitTimeSeconds: options.timeout
     };
 
     self.sqs.receiveMessage(params, function(err, response) {
-      var message;
+      var messages;
 
       if (err) return reject(err);
-      if (!response.Messages) return resolve(null);
+      if (!response.Messages) return resolve(null); // empty
 
-      response = response.Messages[0];
-      message = {
-        id: response.MessageId,
-        body: JSON.parse(response.Body),
-        md5: response.MD5OfBody,
-        receiptHandle: response.ReceiptHandle
-      };
+      messages = response.Messages.map(function (obj) {
+        return {
+          id: obj.MessageId,
+          body: JSON.parse(obj.Body),
+          md5: obj.MD5OfBody,
+          receiptHandle: obj.ReceiptHandle
+        };
+      });
 
-      resolve(message);
+      if (options.limit === 1) {
+        resolve(messages[0]);
+      } else {
+        resolve(messages);
+      }
     });
-
   };
 
   return new Promise(function(resolve, reject) {
@@ -188,11 +198,9 @@ BloodySimpleSQS.prototype.peek = function (options, callback) {
  * @return {Promise}
  */
 BloodySimpleSQS.prototype.remove = function (receiptHandle, callback) {
-  var self = this,
-    resolver;
+  var self = this, resolver;
 
   resolver = function(resolve, reject) {
-
     var params = {
       QueueUrl: self.queueUrl,
       ReceiptHandle: receiptHandle
@@ -203,7 +211,6 @@ BloodySimpleSQS.prototype.remove = function (receiptHandle, callback) {
 
       resolve();
     });
-
   };
 
   return new Promise(function(resolve, reject) {
@@ -221,24 +228,56 @@ BloodySimpleSQS.prototype.remove = function (receiptHandle, callback) {
 /**
  * Retrieves and removes the head of this queue, or returns null if this queue is empty.
  * @param {object} [options] optional request options.
- * @param {function} [callback] an optional call back function, i.e. function (err, message, receiptHandle).
+ * @param {function} [callback] an optional call back function, i.e. function (err, message).
  * @see {@link peek} for further information on the "options" param.
  * @return {Promise}
  */
 BloodySimpleSQS.prototype.poll = function (options, callback) {
-  return this.peek(options).bind(this).then(function (message) {
-    if (message) {
-      return this.remove(message.receiptHandle).then(function () {
-        return message;
-      });
-    }
+  return this.peek(options)
+    .bind(this)
+    .then(function (message) {
+      if (message) {
+        return this.remove(message.receiptHandle)
+          .then(function () {
+            return message;
+          });
+      }
 
-    return null;
-  }).nodeify(callback);
+      return null;
+    })
+    .nodeify(callback);
 };
 
-module.exports = BloodySimpleSQS;
+/**
+ * Appends a new message, with the designated payload, at the end of the queue.
+ * @param {(boolean|string|number|object|null)} payload the message payload.
+ * @param {function} [callback] an optional callback function, i.e. function (err, response).
+ * @return {Promise}
+ */
+BloodySimpleSQS.prototype.createReadStream = function () {
+  var self = this,
+    rs = new Readable({
+      highWaterMark: 1,
+      objectMode: true,
+      encoding: 'utf8'
+    });
 
+  rs._read = function () {
+    self.poll()
+      .then(function (message) {
+        if (!message) return rs.push(null); // end
+        rs.push(message.body);
+      })
+      .catch(function (err) {
+        rs.emit('error', err);
+      });
+  };
+
+  return rs;
+};
+
+
+module.exports = BloodySimpleSQS;
 
 // require('dotenv').load();
 // var sqs = new BloodySimpleSQS({
@@ -247,10 +286,11 @@ module.exports = BloodySimpleSQS;
 //   secretAccessKey: process.env.SQS_SECRET_ACCESS_KEY,
 //   region: process.env.SQS_REGION
 // });
-// // sqs.add(123).then(function (response) {
-// //   console.log(response);
-
-//   sqs.poll().then(function (message) {
-//     console.log(message);
-//   });
-// // });
+// var rs = sqs.createReadStream();
+// rs.on('readable', function() {
+//   setTimeout(function () {
+//     console.log(rs.read())
+//   }, 2000);
+// }).on('error', function (err) {
+//   console.log(err)
+// });
