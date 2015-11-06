5048b3b83079455b4258276a3948b27e8ee79c79
Dimitrios C. Michalakos <dimitris@jmike.gr>
2015-11-06 13:43:27 +0200

    New build system with gulp + babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,22 @@
 var path = require('path');
 var gulp = require('gulp');
-var del = require('del');
-var mkdirp = require('mkdirp');
-var $ = require('gulp-load-plugins')();
+var babel = require('gulp-babel');
+var rimraf = require('gulp-rimraf');
+var plumber = require('gulp-plumber');
 
 var manifest = require('./package.json');
-var mainFile = manifest.main;
-var destinationFolder = path.dirname(mainFile);
+var src = path.resolve(__dirname, 'src');
+var dest = path.resolve(__dirname, path.dirname(manifest.main));
 
-// Remove the built files
-gulp.task('clean', function (cb) {
-  del([destinationFolder], cb);
+gulp.task('clean', function () {
+  return gulp.src(dest, {read: false})
+    .pipe(plumber())
+    .pipe(rimraf());
 });
 
-// Build two versions of the library
-gulp.task('build', ['clean'], function () {
-  mkdirp.sync(destinationFolder);
-  return gulp.src('src/**/*.js')
-    .pipe($.plumber())
-    .pipe($.babel({ blacklist: ['useStrict'] }))
-    .pipe(gulp.dest(destinationFolder));
+gulp.task('build', function () {
+  return gulp.src(path.join(src, './**/*.js'))
+    .pipe(plumber())
+    .pipe(babel())
+    .pipe(gulp.dest(dest));
 });
